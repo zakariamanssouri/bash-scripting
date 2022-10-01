b5505399bc3cd2c808914f8e101c24ab25b841f0
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-01 18:50:13 +0100

    update  find-and-kill-zombie-file.sh script , add instruction for kill parent process id , make the code inside a function

--- find-and-kill-zombie-file.sh
+++ find-and-kill-zombie-file.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ps -aux | awk -F" " '{if($8 == "Z") print $1,$2,$8 }' 
+function getAndKillZombieProcesses(){
+	pids=$(ps -aux | awk -F" " '{if($8 == "Z") print $2}' )  
+	for i in $pids;do
+		ppid=$(ps -o ppid=$i)
+		kill -s SIGCHLD $ppid
+	done
+}
+
+
+getAndKillZombieProcesses
